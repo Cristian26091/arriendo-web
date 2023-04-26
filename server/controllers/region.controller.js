0b9a8cbc8e534a58137ed75d6cbe2c7c9dc447c9
Panda2609 <cbanda15@utalca.cl>
2023-04-26 02:08:37 -0400

    Cambios en el filtro index

--- server/controllers/region.controller.js
+++ server/controllers/region.controller.js
@@ -1,13 +1,21 @@
-const region = require ('../models/region');
+const Region = require ('../models/region');
+
 const regionCtrl = {};
 
+
 regionCtrl.getRegions = async (req, res) => {
-    const regions = await region.find();
+    const regions = await Region.find();
     res.json(regions);
 }
 
-regionCtrl.getRegion = function () {
-
+regionCtrl.getRegion = async (req, res) => {
+    const { regionId } = req.params;
+    const region = await Region.findOne({ _id: regionId });
+    if (!region) {
+        return res.status(404).json({ message: 'Región no encontrada' });
+    }
+    // const comunas = region.comunas.map(comuna => comuna.nombre_comuna);
+    res.json(region);
 }
 
 regionCtrl.createRegion = function() {
--- src/app/auxComponents/searcher/searcher.component.ts
+++ src/app/auxComponents/searcher/searcher.component.ts
@@ -1,4 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { RegionService } from '../../services/region.service';
+import { Region } from '../../models/region.model';
+import { MatSelectChange } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-searcher',
@@ -6,6 +10,55 @@ import { Component} from '@angular/core';
   styleUrls: ['./searcher.component.css']
 })
 export class SearcherComponent{
+  
+  
+  
+  selectedRegion: string;
+  selectedComuna: string;
+  selectedTipoVivienda: string;
 
+  regionesOptions: Region[];
+  comunasOptions: string[];
+  tiposVivienda: string[];
+  
+  constructor(public RegionService: RegionService){
+    this.comunasOptions = [];
+    this.tiposVivienda = [];
+    this.regionesOptions = [];
+  }
+
+  ngOnInit(): void {
+    this.getRegions();
+  }
+
+  getRegions(){
+    this.RegionService.getRegions()
+      .subscribe( res =>{
+        // this.RegionService.regions = res as Region[];
+        this.regionesOptions = res as Region[];
+        console.log(this.regionesOptions);
+    });
+  }
+
+  onRegionSelected(event: any){
+    this.selectedRegion = event.target.value;
+    // console.log(event.target.value);
+    const region = this.regionesOptions.find(r => r.nombre_region === this.selectedRegion);
+    
+      this.comunasOptions = region.comunas.map(c => c.nombre_comuna);
+    
+    console.log(this.comunasOptions)
+
+  }
+
+  onComunaSelected(event: any){
+    this.selectedComuna = event.target.value;
+    // console.log(event.value);
+  }
+
+  onTipoViviendaSelected(event: any){
+    this.selectedTipoVivienda = event.target.value;
+    // console.log(event.value);
+  }
 
 }
