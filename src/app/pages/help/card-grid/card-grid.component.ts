63d697c9e02068172e88188bc62957ed31a741dd
Panda2609 <cbanda15@utalca.cl>
2023-06-05 02:29:49 -0400

    cambio en las cards de ayuda

--- src/app/pages/help/card-grid/card-grid.component.ts
+++ src/app/pages/help/card-grid/card-grid.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardGridComponent implements OnInit {
 
+  cards = [
+    { title: 'Card 1', content: 'This is the content of card 1.' },
+    { title: 'Card 2', content: 'This is the content of card 2.' },
+    { title: 'Card 3', content: 'This is the content of card 3.' },
+    { title: 'Card 4', content: 'This is the content of card 4.' },
+    // Agrega más tarjetas si es necesario
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
