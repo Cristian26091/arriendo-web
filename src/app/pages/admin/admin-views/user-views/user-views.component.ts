835259b25649ea1d9749009cca877ef137a0b861
Panda2609 <cbanda15@utalca.cl>
2023-08-21 02:53:09 -0400

    vista inicial del panel de administrador para reserva y usuario

--- src/app/pages/admin/admin-views/user-views/user-views.component.ts
+++ src/app/pages/admin/admin-views/user-views/user-views.component.ts
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-views.component.css']
 })
 export class UserViewsComponent implements OnInit {
-
-  constructor() { }
+  currentRoute: string = "";
+  currentRouteParts: string[] = ["primero", "segundo"];
+  headTableContent: string[];
+  data = [
+    {
+      id: 1,
+      nombre: "Juan Pérez",
+      email: "juan.perez@email.com",
+      telefono: "555-123-4567",
+      rut: "12.345.678-9"
+    },
+    {
+      id: 2,
+      nombre: "María Rodríguez",
+      email: "maria.rodriguez@email.com",
+      telefono: "555-987-6543",
+      rut: "98.765.432-1"
+    },
+    {
+      id: 3,
+      nombre: "Carlos Sánchez",
+      email: "carlos.sanchez@email.com",
+      telefono: "555-567-8901",
+      rut: "11.223.344-5"
+    },
+    // Agrega más usuarios según sea necesario
+  ];
+  constructor() { 
+    this.headTableContent = ["ID", "Nombre", "Email", "Telefono", "Rut", "Acción"];
+  }
 
   ngOnInit(): void {
+  }
+
+  editarItem(item){
+    console.log(item);
+  }
+
+  eliminarItem(item){
+    console.log(item);
   }
 
 }
