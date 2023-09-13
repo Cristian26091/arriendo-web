5defde0fda88878f75bca39542bffe8198643718
Panda2609 <cbanda15@utalca.cl>
2023-09-13 19:32:05 -0300

    Preparacion de datos para las reservas

--- src/app/models/room.ts
+++ src/app/models/room.ts
@@ -1,9 +1,10 @@
-export class Room {
+import { Booking } from './booking'; // Importamos el modelo de reserva
 
+export class Room {
     constructor(_id='', id_region='', latitud=0, longitud=0, banio_compartido=null, 
     descripcion='', fecha_publicacion = null, region = '', ciudad = '',
     comuna = '', calle = '', numero = '', casa_depto = '', precio = '', url_image_cover = '', esta_arrendado = '',
-    url_model = '', url_texture = ''
+    url_model = '', url_texture = '', reservas = []
     ){
         this._id = _id;
         this.id_region = id_region;
@@ -23,6 +24,7 @@ export class Room {
         this.url_model = url_model;
         this.url_texture = url_texture;
         this.esta_arrendado = esta_arrendado;
+        this.reservas = reservas; // Campo para gestionar reservas
     }
 
     _id: string;
@@ -43,4 +45,5 @@ export class Room {
     url_model: String
     url_texture: String
     esta_arrendado: String
+    reservas: Booking[]; // Cambiamos el tipo de dato a un array de objetos Reserva
 }
