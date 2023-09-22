1c1fbb185f7b402c1e06db220b96db9f68ccccca
Panda2609 <cbanda15@utalca.cl>
2023-09-22 17:11:04 -0300

    preparación para postear la habitación

--- src/app/pages/admin/admin-views/room-views/room-views-add/room-views-add/room-views-add.component.ts
+++ src/app/pages/admin/admin-views/room-views/room-views-add/room-views-add/room-views-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RoomService } from 'src/app/services/room.service';
 
 @Component({
   selector: 'app-room-views-add',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoomViewsAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(private roomService: RoomService) { }
 
   ngOnInit(): void {
   }
