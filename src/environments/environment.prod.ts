0d23ff7bffdc5ea6610bc67ef9be1e954b012e1a
Panda2609 <cbanda15@utalca.cl>
2023-11-23 19:53:03 -0300

    cambio de ip estatica para el servidor web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   mapBoxToken: 'pk.eyJ1IjoicGFuZGEyNjA5IiwiYSI6ImNsb2N4eTVwNTAyY24ycW11emltd3pzZDEifQ.EEfpiZzkXGPKoTutR0x8Rw',
-  uri: 'http://34.176.11.75:3000',
+  uri: 'http://34.176.106.128:3000',
   secretToken: 'mysecrettoken',
   estado: {
     pendiente: 'pendiente',
