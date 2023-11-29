7a303e188947f41fa7d232198e87378b7d6a8879
Panda2609 <cbanda15@utalca.cl>
2023-11-29 17:17:38 -0300

    cambios en la uri del environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,8 @@
 export const environment = {
     production: false,
     mapBoxToken: 'pk.eyJ1IjoicGFuZGEyNjA5IiwiYSI6ImNsb2N4eTVwNTAyY24ycW11emltd3pzZDEifQ.EEfpiZzkXGPKoTutR0x8Rw',
-    uri: 'http://127.0.0.1:3000',
+    // uri: 'http://127.0.0.1:3000',
+    uri: 'https://34.176.106.128:3000',
     secretToken: 'mysecrettoken',
     estado: {
         pendiente: 'pendiente',
